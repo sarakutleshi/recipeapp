ddca22c2543192b4239409204d3c4614789d35d8
SaraKutleshi <sarakutleshi@gmail.com>
2025-01-02 20:51:37 +0100

    modified UserService

--- src/main/java/dev/sarakutleshi/recipeapp/services/UserService.java
+++ src/main/java/dev/sarakutleshi/recipeapp/services/UserService.java
@@ -1,10 +1,12 @@
 package dev.sarakutleshi.recipeapp.services;
 
-import dev.sarakutleshi.recipeapp.dtos.UserDto;
 import org.springframework.stereotype.Service;
+
+import dev.sarakutleshi.recipeapp.dtos.RegisterUserRequestDto;
+import dev.sarakutleshi.recipeapp.dtos.UserDto;
 
 @Service
 public interface UserService {
     UserDto login(String email, String password);
-//    boolean register(RegisterUserRequestDto registerUserRequestDto);
+    boolean register(RegisterUserRequestDto registerUserRequestDto);
 }
