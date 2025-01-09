d9862de28cc27e10a5e5c689aa71febe753f1559
sarakutleshi <Sara.kutleshi@student.cacttus.education>
2025-01-09 13:28:26 +0100

    "Updating"

--- src/main/java/dev/sarakutleshi/recipeapp/mappers/UserMapper.java
+++ src/main/java/dev/sarakutleshi/recipeapp/mappers/UserMapper.java
@@ -5,7 +5,6 @@ import dev.sarakutleshi.recipeapp.dtos.UserDto;
 import dev.sarakutleshi.recipeapp.dtos.UserListingDto;
 import dev.sarakutleshi.recipeapp.models.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper extends Convert<User, UserDto> {
