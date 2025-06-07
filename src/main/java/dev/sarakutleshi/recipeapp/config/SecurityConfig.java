29ee96bd1fbd29e421849f500f6ae403b07e4aeb
sarakutleshi <Sara.kutleshi@student.cacttus.education>
2025-06-07 16:47:13 +0200

    add CORS configuration and enable CORS in security settings

--- src/main/java/dev/sarakutleshi/recipeapp/config/SecurityConfig.java
+++ src/main/java/dev/sarakutleshi/recipeapp/config/SecurityConfig.java
@@ -1,18 +1,68 @@
 package dev.sarakutleshi.recipeapp.config;
 
 
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+import dev.sarakutleshi.recipeapp.models.User;
+import dev.sarakutleshi.recipeapp.repositories.UserRepository;
+import dev.sarakutleshi.recipeapp.security.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static dev.sarakutleshi.recipeapp.models.Role.USER;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/recipes/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/log-in").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/sign-up").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository repository) {
+        AppUserDetailsService appUserDetailsService = new AppUserDetailsService(repository);
+
+        String email = "user@test.com";
+        repository.findByEmail(email)
+                .orElseGet(() -> {
+                    var newUser = User.builder()
+                            .username(email)
+                            .password(passwordEncoder().encode("password"))
+                            .build();
+                    return repository.save(newUser);
+                });
+
+        return appUserDetailsService;
     }
 }
